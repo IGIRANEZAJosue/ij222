5e10bcd6f9660b3fc397b041e07f4ddc47a74ca8
IGIRANEZA Josue <igiranezaj28@gmail.com>
2023-04-28 20:31:41 +0300

    commit

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -2,8 +2,10 @@ import React from 'react'
 
 const Home = () => {
   return (
-    <div className='p-4 w-[83vw]'>
-      <h1 className='text-4xl font-semibold mb-2'>This s Home</h1>
+    <div className='p-4 w-[83vw] h-[100vh] flex justify-center items-center'>
+      <h1 className='text-4xl font-bold mb-2 text-center'>
+        Welcome to the <br /> <span className="text-6xl font-montserrat font-extrabold text-primary">NYUNGURA</span> <br /> Dashboard
+      </h1>
     </div>
   )
 }
