e6a03597992c7fce4eb6851b93f11b69f5795e0c
IGIRANEZA Josue <igiranezaj28@gmail.com>
2023-04-30 14:11:23 +0300

    ...

--- src/components/PaymentCards.jsx
+++ src/components/PaymentCards.jsx
@@ -16,7 +16,7 @@ const PaymentCards = () => {
       </div>
 
       <div className="flex-1 p-4 bg-white rounded-xl drop-shadow-2xl hover:drop-shadow-sm duration-300 ">
-         <h1 className="flex items-center gap-4 font-semibold text-xl mb-2"> <i class="fa-light fa-money-bill-wave text-lg"></i> Total Loans</h1>
+         <h1 className="flex items-center gap-4 font-semibold text-xl mb-2"> <i class="fa-regular fa-hand-holding-dollar text-[24px]"></i> Total Loans</h1>
          <div className="flex justify-between items-center font-semibold text-2xl">
             <p>$8000</p>
             <div className="radial-progress text-[#00bfa6]/80" style={{"--value":74}}>74%</div>
@@ -25,7 +25,7 @@ const PaymentCards = () => {
       </div>
 
       <div className="flex-1 p-4 bg-white rounded-xl drop-shadow-2xl hover:drop-shadow-sm duration-300  ">
-         <h1 className="flex items-center gap-4 font-semibold text-xl mb-2"> <i class="fa-light fa-money-bill-wave text-lg"></i> Total Shares</h1>
+         <h1 className="flex items-center gap-4 font-semibold text-xl mb-2"> <i class="fa-regular fa-money-bill-wave text-lg"></i> Total Shares</h1>
          <div className="flex justify-between items-center font-semibold text-2xl">
             <p>$8000</p>
             <div className="radial-progress text-[#dc2626]" style={{"--value":65}}>65%</div>
